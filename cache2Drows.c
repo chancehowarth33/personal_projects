8e7ebdcb23cde03c530b2a05cd6e84db687bcce8
chancehowarth33 <155261337+chancehowarth33@users.noreply.github.com>
2023-12-30 14:40:28 -0600

    Update cache2Drows.c

--- cache2Drows.c
+++ cache2Drows.c
@@ -1,24 +1,6 @@
-////////////////////////////////////////////////////////////////////////////////
-// Main File:        cache1D.c
-// This File:        cache2Drows.c
-// Other Files:      cache1D.c, cache2Dcols.c, cache2Dclash.c
-// Semester:         CS 354 Lecture 002 Fall 2023
-// Grade Group:      gg12
-// 
-// Author:           Chance Howarth
-// Email:            crhowarth@wisc.edu
-// CS Login:         howarth
-//
-///////////////////////////  WORK LOG  //////////////////////////////
-//  Document your work sessions on your copy http://tiny.cc/work-log
-//  Download and submit a pdf of your work log for each project.
-/////////////////////////// OTHER SOURCES OF HELP ////////////////////////////// 
-// Persons:          N/A
-//
-// Online sources:   N/A
-// 
-// AI chats:         N/A
-//////////////////////////// 80 columns wide ///////////////////////////////////
+// Author: Chance Howarth
+// Email: howarthchance@gmail.com
+
 #include <stdio.h>
 
 #define X 3000
