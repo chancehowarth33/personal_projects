0085740a86b4944411a1c176c7a16b49ece78a5a
chancehowarth33 <155261337+chancehowarth33@users.noreply.github.com>
2023-12-30 14:40:12 -0600

    Update cache2Dclash.c

--- cache2Dclash.c
+++ cache2Dclash.c
@@ -1,25 +1,5 @@
-////////////////////////////////////////////////////////////////////////////////
-// Main File:        cache2Dclash.c
-// This File:        cache2Dclash.c
-// Other Files:      cache2Drows.c, cache2Dcols.c, cache1D.c
-// Semester:         CS 354 Lecture 002 Fall 2023
-// Grade Group:      gg12
-// Instructor:       deppeler
-// 
-// Author:           Chance Howarth
-// Email:            crhowarth@wisc.edu
-// CS Login:         howarth
-//
-///////////////////////////  WORK LOG  //////////////////////////////
-//  Document your work sessions on your copy http://tiny.cc/work-log
-//  Download and submit a pdf of your work log for each project.
-/////////////////////////// OTHER SOURCES OF HELP ////////////////////////////// 
-// Persons:          N/A
-//
-// Online sources:   N/A
-// 
-// AI chats:         N/A
-//////////////////////////// 80 columns wide ///////////////////////////////////
+// Author: Chance Howarth
+// Email: howarthchance@gmail.com
 
 #include <stdio.h>
 
