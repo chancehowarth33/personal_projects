1c9adf830297f61b1f949b6be458944403159d7e
chancehowarth33 <155261337+chancehowarth33@users.noreply.github.com>
2023-12-30 17:39:38 -0600

    Update DijkstraGraph.java

--- flight_router/DijkstraGraph.java
+++ flight_router/DijkstraGraph.java
@@ -1,41 +1,18 @@
-// --== CS400 File Header Information ==--
-// Name: Chance Howarth
-// Email: crhowarth@wisc.edu
-// Group and Team: G17
-// Group TA: Robert Nagel
-// Lecturer: Florian
-// Notes to Grader: N/A
+// Author: Chance Howarth
+// Email: howarthchance@gmail.com
 
 
 import java.util.PriorityQueue;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * This class extends the BaseGraph data structure with additional methods for
- * computing the total cost and list of node data along the shortest path
- * connecting a provided starting to ending nodes. This class makes use of
- * Dijkstra's shortest path algorithm.
- */
+
 public class DijkstraGraph<NodeType, EdgeType extends Number>
         extends BaseGraph<NodeType, EdgeType>
         implements GraphADT<NodeType, EdgeType> {
 
-    /**
-     * While searching for the shortest path between two nodes, a SearchNode
-     * contains data about one specific path between the start node and another
-     * node in the graph. The final node in this path is stored in its node
-     * field. The total cost of this path is stored in its cost field. And the
-     * predecessor SearchNode within this path is referenced by the predecessor
-     * field (this field is null within the SearchNode containing the starting
-     * node in its node field).
-     *
-     * SearchNodes are Comparable and are sorted by cost so that the lowest cost
-     * SearchNode has the highest priority within a java.util.PriorityQueue.
-     */
+
     protected class SearchNode implements Comparable<SearchNode> {
         public Node node;
         public double cost;
@@ -67,18 +44,7 @@ public class DijkstraGraph<NodeType, EdgeType extends Number>
 
     /**
      * This helper method creates a network of SearchNodes while computing the
-     * shortest path between the provided start and end locations. The
-     * SearchNode that is returned by this method is represents the end of the
-     * shortest path that is found: it's cost is the cost of that shortest path,
-     * and the nodes linked together through predecessor references represent
-     * all of the nodes along that shortest path (ordered from end to start).
-     *
-     * @param start the data item in the starting node for the path
-     * @param end   the data item in the destination node for the path
-     * @return SearchNode for the final end node within the shortest path
-     * @throws NoSuchElementException when no path from start to end is found
-     *                                or when either start or end data do not
-     *                                correspond to a graph node
+     * shortest path between the provided start and end locations.
      */
 
     protected SearchNode computeShortestPath(NodeType start, NodeType end) throws NoSuchElementException {
@@ -127,14 +93,7 @@ public class DijkstraGraph<NodeType, EdgeType extends Number>
     /**
      * Returns the list of data values from nodes along the shortest path
      * from the node with the provided start value through the node with the
-     * provided end value. This list of data values starts with the start
-     * value, ends with the end value, and contains intermediary values in the
-     * order they are encountered while traversing this shorteset path. This
-     * method uses Dijkstra's shortest path algorithm to find this solution.
-     *
-     * @param start the data item in the starting node for the path
-     * @param end   the data item in the destination node for the path
-     * @return list of data item from node along this shortest path
+     * provided end value.
      */
     public List<NodeType> shortestPathData(NodeType start, NodeType end) {
         SearchNode endNode = computeShortestPath(start, end);
@@ -148,13 +107,8 @@ public class DijkstraGraph<NodeType, EdgeType extends Number>
     /**
      * Returns the cost of the path (sum over edge weights) of the shortest
      * path freom the node containing the start data to the node containing the
-     * end data. This method uses Dijkstra's shortest path algorithm to find
-     * this solution.
-     *
-     * @param start the data item in the starting node for the path
-     * @param end   the data item in the destination node for the path
-     * @return the cost of the shortest path between these nodes
-     */
+     * end data.
+*/
     public double shortestPathCost(NodeType start, NodeType end) {
         SearchNode endNode = computeShortestPath(start, end);
         return endNode.cost;
